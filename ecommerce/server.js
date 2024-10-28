4750599b71ecd1a2c6c0fdf72942eea062cf360d
Chaitanya <chaitanyaasole@gmail.com>
2024-10-28 20:12:58 +0530

    app.listen

--- ecommerce/server.js
+++ ecommerce/server.js
@@ -9,3 +9,13 @@ app.get("/", (req, res) => {
     message: "Welcome to ecommerce app",
   });
 });
+
+// PORT
+const PORT = 8080;
+
+// For React => PORT = 3000
+// For Angular => PORT = 4200
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
