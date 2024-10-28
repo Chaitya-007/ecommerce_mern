85918ef1ef5d8dcecb89838fb0e1d1dc68a26d11
Chaitanya <chaitanyaasole@gmail.com>
2024-10-28 21:08:56 +0530

    added colors to mongodb

--- ecommerce/config/db.js
+++ ecommerce/config/db.js
@@ -5,10 +5,10 @@ const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URL);
     console.log(
-      `Connected To Mongodb Database ${conn.connection.host}`.cyan.underline
+      `Connected To Mongodb Database ${conn.connection.host}`.bgMagenta.white
     );
   } catch (error) {
-    console.log(`Error in Mongodb: ${error}`.red.underline.bold);
+    console.log(`Error in Mongodb: ${error}`.bgRed.white);
   }
 };
 
