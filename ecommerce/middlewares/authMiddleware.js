595f0ff422a789a9a9573ecfc912a60da768a1de
Chaitanya <chaitanyaasole@gmail.com>
2024-11-03 21:43:02 +0530

    Made middleware for admin

--- ecommerce/middlewares/authMiddleware.js
+++ ecommerce/middlewares/authMiddleware.js
@@ -1,4 +1,5 @@
 import JWT from "jsonwebtoken";
+import userSchema from "../models/userModel.js";
 
 // Protected Routes Token Base
 export const requireSignIn = async (req, res, next) => {
@@ -8,6 +9,21 @@ export const requireSignIn = async (req, res, next) => {
       process.env.JWT_SECRET
     );
     next();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const isAdmin = async (req, res, next) => {
+  try {
+    const user = await userSchema.findById(req.user._id);
+    if (user !== 1) {
+      return res
+        .status(401)
+        .send({ success: false, message: "UnAuthorized Access" });
+    } else {
+      next();
+    }
   } catch (error) {
     console.log(error);
   }
