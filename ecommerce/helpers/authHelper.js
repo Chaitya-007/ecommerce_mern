424db5ff3dd4a96fc1f971c943a43836de8cf7cb
Chaitanya <chaitanyaasole@gmail.com>
2024-11-03 17:33:15 +0530

    Made hash functioinality using bcrypt functionality

---
+++ ecommerce/helpers/authHelper.js
@@ -0,0 +1,12 @@
+import bcrypt from "bcrypt";
+
+export const hashPassword = async (password) => {
+  try {
+    // a value of 10 means 2^10 times the iteration will happen
+    const saltrounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltrounds);
+    return hashedPassword;
+  } catch (error) {
+    console.log(error);
+  }
+};
