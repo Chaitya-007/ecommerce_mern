21e2d1a08efa1e342e5f9ba61a8af19c2d746141
Chaitanya <chaitanyaasole@gmail.com>
2024-11-04 09:50:33 +0530

    Using children components

--- ecommerce/client/src/App.js
+++ ecommerce/client/src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
+import Layout from "./components/Layout/Layout";
 
 function App() {
   return (
-    <div>
-      <h1>App Component</h1>
-    </div>
+    <>
+      <Layout>
+        <h1>Ecommerce App </h1>
+      </Layout>
+    </>
   );
 }
 
