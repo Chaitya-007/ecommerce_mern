6e5893f86c94a9af3323cde40f7dde0d5a6ddcb5
Chaitanya <chaitanyaasole@gmail.com>
2024-11-04 10:01:15 +0530

    Designed footer

--- ecommerce/client/src/components/Layout/Footer.js
+++ ecommerce/client/src/components/Layout/Footer.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div>
-      <h1>Footer</h1>
+    <div className="bg-dark text-white p-3">
+      <h4 className="text-center">All Rights Reseved &copy; Chaitya</h4>
     </div>
   );
 };
