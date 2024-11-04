9f34b5737c55cb778becd4878a81fc08729f2366
Chaitanya <chaitanyaasole@gmail.com>
2024-11-04 16:34:30 +0530

    Adding react-toastify

--- ecommerce/client/src/pages/Auth/Register.js
+++ ecommerce/client/src/pages/Auth/Register.js
@@ -1,19 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import Layout from "./../../components/Layout/Layout";
 
 const Register = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [phone, setPhone] = useState("");
+  const [address, setAddress] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(name, email, password, phone, address);
+  };
+
   return (
     <Layout title={"Regiser Ecommerce App"}>
       <div className="register">
         <h1>Register</h1>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="mb-3">
             <input
               type="text"
+              value={name}
               className="form-control"
-              id="exampleInputEmail1"
-              aria-describedby="emailHelp"
+              id="exampleInputName"
               placeholder="Enter Name"
+              onChange={(e) => setName(e.target.value)}
+              required
             />
           </div>
           <div className="mb-3">
@@ -21,34 +34,43 @@ const Register = () => {
               type="email"
               className="form-control"
               id="exampleInputEmail1"
-              aria-describedby="emailHelp"
+              value={email}
               placeholder="Enter Email"
+              onChange={(e) => setEmail(e.target.value)}
+              required
             />
           </div>
           <div className="mb-3">
             <input
               type="password"
               className="form-control"
-              id="exampleInputPassword1"
+              id="exampleInputPassword"
               placeholder="Enter Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
             />
           </div>
           <div className="mb-3">
             <input
               type="text"
               className="form-control"
-              id="exampleInputEmail1"
-              aria-describedby="emailHelp"
+              id="exampleInputPhone"
               placeholder="Enter Phone"
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
+              required
             />
           </div>
           <div className="mb-3">
             <input
               type="text"
               className="form-control"
-              id="exampleInputEmail1"
-              aria-describedby="emailHelp"
+              id="exampleInputAddress"
+              value={address}
               placeholder="Enter Address"
+              onChange={(e) => setAddress(e.target.value)}
+              required
             />
           </div>
 
