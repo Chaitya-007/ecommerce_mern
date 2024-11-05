6b72419d90de9a601d2aca69e8b3237f008d8aa1
Chaitanya <chaitanyaasole@gmail.com>
2024-11-05 18:21:58 +0530

    Learn how to protect route and how to nest route using outlet

--- ecommerce/client/src/pages/user/Dashboard.js
+++ ecommerce/client/src/pages/user/Dashboard.js
@@ -1,7 +1,11 @@
 import React from "react";
 import Layout from "../../components/Layout/Layout";
+import axios from "axios";
+import { useAuth } from "../../context/auth";
 
 const Dashboard = () => {
+  const [auth, setAuth] = useAuth();
+
   return (
     <Layout title={"Dashboard - Ecommerce App"}>
       <h1>Dashboard Page</h1>
